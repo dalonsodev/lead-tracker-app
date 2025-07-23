58e94173bc1a2c1649c8946f113601e0958d3607
dalonso <masdavidalonso@gmail.com>
2025-07-23 14:29:00 +0200

    Refactor Firebase initialization and event listeners
    Added explanatory comments for Firebase setup and database references. Replaced variable-based button references with direct DOM queries in event listeners for improved clarity and maintainability.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// importar funciones desde Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
 import { getDatabase,
          ref,
@@ -5,18 +6,21 @@ import { getDatabase,
          onValue,
          remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
+// Configuración de Firebase con la URL de la base de datos en tiempo real para almacenar leads
 const firebaseConfig = {
    databaseURL: "https://leads-tracker-app-4577d-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
+// Inicializa Firebase con la configuración de la base de datos en tiempo real
 const app = initializeApp(firebaseConfig)
+// Obtiene la instancia de la base de datos
 const database = getDatabase(app)
+// Referencia al nodo "leads" donde se almacenan las URLs
 const referenceInDB = ref(database, "leads")
 
+
 const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
 
 function render(leads) {
    let listItems = ""
@@ -41,12 +45,12 @@ onValue(referenceInDB, function(snapshot) {
    }
 })
 
-deleteBtn.addEventListener("dblclick", function() {
+document.getElementById("delete-btn").addEventListener("dblclick", function() {
    remove(referenceInDB)
    ulEl.innerHTML = ""
 })
 
-inputBtn.addEventListener("click", function() {
+document.getElementById("input-btn").addEventListener("click", function() {
    push(referenceInDB, inputEl.value)
    inputEl.value = "" 
 })
